01041f880b7493cd884bfa6c7fd50597bce79707
nandoti <nando.frontend@gmail.com>
2022-10-14 12:25:42 -0300

    feat: criação do app

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            "module-resolver", {
+                root: ["./src"],
+                extensions: [".js", ".jsx", ".ts", ".tsx",],
+                alias: {
+                    "@assets": "./src/assets",
+                    "@components": "./src/components",
+                    "@config": "./src/config",
+                    "@hooks": "./src/hooks",
+                    "@screens": "./src/screens",
+                    "@services": "./src/services",
+                    "@utils": "./src/utils",
+                },
+            },
+        ],
+    };
 };
